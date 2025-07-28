bf1b41bb5ffd29b374a0435cedc1332861f749f6
David <20240273@ricaldone.edu.sv>
2025-07-28 10:28:03 -0600

    Hola

---
+++ src/main/java/DavidGuardado_20240273/DavidGuardado_20240273/ProyectoModuloDavidGuardado20240273Application.java
@@ -0,0 +1,13 @@
+package DavidGuardado_20240273.DavidGuardado_20240273;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoModuloDavidGuardado20240273Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoModuloDavidGuardado20240273Application.class, args);
+	}
+
+}
---
+++ src/test/java/DavidGuardado_20240273/DavidGuardado_20240273/ProyectoModuloDavidGuardado20240273ApplicationTests.java
@@ -0,0 +1,13 @@
+package DavidGuardado_20240273.DavidGuardado_20240273;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoModuloDavidGuardado20240273ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
